538797ad605d9f242c16620c5bda175f55cbfe5d
Jasmine Mithani <jazzmithani@gmail.com>
2019-09-07 12:18:46 -0500

    initial commit

---
+++ turnout.js
@@ -0,0 +1,236 @@
+turnout = [
+  {
+    "precinct": 1,
+    "registered_voters": 659,
+    "ballots_cast": 183,
+    "turnout": 27.77
+  },
+  {
+    "precinct": 2,
+    "registered_voters": 471,
+    "ballots_cast": 108,
+    "turnout": 22.93
+  },
+  {
+    "precinct": 3,
+    "registered_voters": 653,
+    "ballots_cast": 177,
+    "turnout": 27.11
+  },
+  {
+    "precinct": 4,
+    "registered_voters": 507,
+    "ballots_cast": 91,
+    "turnout": 17.95
+  },
+  {
+    "precinct": 5,
+    "registered_voters": 811,
+    "ballots_cast": 225,
+    "turnout": 27.74
+  },
+  {
+    "precinct": 6,
+    "registered_voters": 506,
+    "ballots_cast": 97,
+    "turnout": 19.17
+  },
+  {
+    "precinct": 7,
+    "registered_voters": 866,
+    "ballots_cast": 178,
+    "turnout": 20.55
+  },
+  {
+    "precinct": 8,
+    "registered_voters": 691,
+    "ballots_cast": 236,
+    "turnout": 34.15
+  },
+  {
+    "precinct": 9,
+    "registered_voters": 776,
+    "ballots_cast": 285,
+    "turnout": 36.73
+  },
+  {
+    "precinct": 10,
+    "registered_voters": 660,
+    "ballots_cast": 165,
+    "turnout": 25
+  },
+  {
+    "precinct": 11,
+    "registered_voters": 941,
+    "ballots_cast": 254,
+    "turnout": 26.99
+  },
+  {
+    "precinct": 12,
+    "registered_voters": 642,
+    "ballots_cast": 123,
+    "turnout": 19.16
+  },
+  {
+    "precinct": 13,
+    "registered_voters": 1000,
+    "ballots_cast": 251,
+    "turnout": 25.1
+  },
+  {
+    "precinct": 14,
+    "registered_voters": 412,
+    "ballots_cast": 171,
+    "turnout": 41.5
+  },
+  {
+    "precinct": 15,
+    "registered_voters": 835,
+    "ballots_cast": 148,
+    "turnout": 17.72
+  },
+  {
+    "precinct": 16,
+    "registered_voters": 690,
+    "ballots_cast": 261,
+    "turnout": 37.83
+  },
+  {
+    "precinct": 17,
+    "registered_voters": 666,
+    "ballots_cast": 135,
+    "turnout": 20.27
+  },
+  {
+    "precinct": 18,
+    "registered_voters": 663,
+    "ballots_cast": 174,
+    "turnout": 26.24
+  },
+  {
+    "precinct": 19,
+    "registered_voters": 796,
+    "ballots_cast": 227,
+    "turnout": 28.52
+  },
+  {
+    "precinct": 20,
+    "registered_voters": 635,
+    "ballots_cast": 126,
+    "turnout": 19.84
+  },
+  {
+    "precinct": 21,
+    "registered_voters": 742,
+    "ballots_cast": 163,
+    "turnout": 21.97
+  },
+  {
+    "precinct": 22,
+    "registered_voters": 744,
+    "ballots_cast": 300,
+    "turnout": 40.32
+  },
+  {
+    "precinct": 23,
+    "registered_voters": 831,
+    "ballots_cast": 254,
+    "turnout": 30.57
+  },
+  {
+    "precinct": 24,
+    "registered_voters": 670,
+    "ballots_cast": 215,
+    "turnout": 32.09
+  },
+  {
+    "precinct": 25,
+    "registered_voters": 717,
+    "ballots_cast": 193,
+    "turnout": 26.92
+  },
+  {
+    "precinct": 26,
+    "registered_voters": 602,
+    "ballots_cast": 242,
+    "turnout": 40.2
+  },
+  {
+    "precinct": 27,
+    "registered_voters": 523,
+    "ballots_cast": 145,
+    "turnout": 27.72
+  },
+  {
+    "precinct": 28,
+    "registered_voters": 560,
+    "ballots_cast": 170,
+    "turnout": 30.36
+  },
+  {
+    "precinct": 29,
+    "registered_voters": 883,
+    "ballots_cast": 298,
+    "turnout": 33.75
+  },
+  {
+    "precinct": 30,
+    "registered_voters": 427,
+    "ballots_cast": 114,
+    "turnout": 26.7
+  },
+  {
+    "precinct": 31,
+    "registered_voters": 832,
+    "ballots_cast": 223,
+    "turnout": 26.8
+  },
+  {
+    "precinct": 32,
+    "registered_voters": 858,
+    "ballots_cast": 228,
+    "turnout": 26.57
+  },
+  {
+    "precinct": 33,
+    "registered_voters": 579,
+    "ballots_cast": 136,
+    "turnout": 23.49
+  },
+  {
+    "precinct": 34,
+    "registered_voters": 675,
+    "ballots_cast": 274,
+    "turnout": 40.59
+  },
+  {
+    "precinct": 35,
+    "registered_voters": 914,
+    "ballots_cast": 141,
+    "turnout": 15.43
+  },
+  {
+    "precinct": 36,
+    "registered_voters": 379,
+    "ballots_cast": 81,
+    "turnout": 21.37
+  },
+  {
+    "precinct": 37,
+    "registered_voters": 303,
+    "ballots_cast": 80,
+    "turnout": 26.4
+  },
+  {
+    "precinct": 38,
+    "registered_voters": 380,
+    "ballots_cast": 92,
+    "turnout": 24.21
+  },
+  {
+    "precinct": 39,
+    "registered_voters": 163,
+    "ballots_cast": 37,
+    "turnout": 22.7
+  }
+];
